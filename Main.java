2d7eb4ec53052497150ced1ef540c55a131b9beb
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 13:33:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+   This program demonstrates how a superclass
+   constructor is called with the super key word.
+*/
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      SubClass2 obj = new SubClass2();
+   }
+}
